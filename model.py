928c5e844381e6fe537cf8058da79d4afcb8b366
dh610 <ddhhyy2000@gmail.com>
2024-09-25 16:09:22 +0900

    size modify

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 
 class CNNModel(nn.Module):
@@ -7,15 +6,20 @@ class CNNModel(nn.Module):
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(32, 16, kernel_size=3, padding=1)
+        self.conv5 = nn.Conv2d(16, 8, kernel_size=3, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(32 * 8 * 8, 32)
+        self.relu = nn.ReLU()
+        self.fc1 = nn.Linear(8 * 8 * 8, 32)
         self.fc2 = nn.Linear(32, num_classes)
 
     def forward(self, x):
-        x = self.pool(torch.relu(self.conv1(x)))
-        x = self.pool(torch.relu(self.conv2(x)))
-        x = self.pool(torch.relu(self.conv3(x)))
-        x = x.view(-1, 32 * 8 * 8)
-        x = torch.relu(self.fc1(x))
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = self.pool(self.relu(self.conv3(x)))
+        x = self.pool(self.relu(self.conv4(x)))
+        x = self.pool(self.relu(self.conv5(x)))
+        x = x.view(-1, 8 * 8 * 8)
+        x = self.relu(self.fc1(x))
         x = self.fc2(x)
         return x
