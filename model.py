4d5d9c3366e67b7ed5b3d04dc9f3bf02dcafa115
dh610 <ddhhyy2000@gmail.com>
2024-09-25 17:30:27 +0900

    model

--- model.py
+++ model.py
@@ -16,6 +16,7 @@ class MyModel(nn.Module):
         x = self.pool(self.relu(self.conv1(x)))
         x = self.pool(self.relu(self.conv2(x)))
         x = self.pool(self.relu(self.conv3(x)))
+        print(x)
         x = x.view(x.size(0), -1)
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
