bdb89a5a57673c27fabb9f0b25f004c804569686
dh610 <ddhhyy2000@gmail.com>
2024-09-25 17:23:16 +0900

    model

--- model.py
+++ model.py
@@ -6,11 +6,9 @@ class MyModel(nn.Module):
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(32, 16, kernel_size=3, padding=1)
-        self.conv5 = nn.Conv2d(16, 8, kernel_size=3, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(8 * 32 * 32, 32)
+        self.fc1 = nn.Linear(32 * 8 * 8, 32)
         self.fc2 = nn.Linear(32, num_classes)
         self.dropout = nn.Dropout()
 
@@ -18,9 +16,7 @@ class MyModel(nn.Module):
         x = self.pool(self.relu(self.conv1(x)))
         x = self.pool(self.relu(self.conv2(x)))
         x = self.pool(self.relu(self.conv3(x)))
-        x = self.pool(self.relu(self.conv4(x)))
-        x = self.pool(self.relu(self.conv5(x)))
-        x = x.view(-1, 8 * 32 * 32)
+        x = x.view(-1, 32 * 8 * 8)
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
         return x
