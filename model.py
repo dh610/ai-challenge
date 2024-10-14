0616719fd4b9ff43da629d85e065d89689574e6a
dh610 <ddhhyy2000@gmail.com>
2024-10-14 16:14:55 +0900

    model

--- model.py
+++ model.py
@@ -1,27 +1,60 @@
 import torch
 import torch.nn as nn
 
-class MobileConv(nn.Module):
+# Depthwisw Separable Convolution
+class DSC(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1, kernel_size=3, padding=0, bias=False):
-        super(MobileConv, self).__init__()
+        super(DSC, self).__init__()
+        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size, stride, padding, groups=in_channels, bias=bias)
+        self.pointwise = nn.Conv2d(in_channels, out_channels, 1, bias=bias)
 
     def forward(self, x):
-        return x
+        x = self.depthwise(x)
+        return self.pointwise(x)
 
+    
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(BasicBlock, self).__init__()
+        
+        self.residual_function = nn.Sequential(
+            DSC(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            DSC(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+        )
+
+        self.relu = nn.ReLU(inplace=True)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                DSC(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        x = self.residual_function(x) + self.shortcut(x)
+        return self.relu(x)
+
+'''
 class BottleNeck(nn.Module):
 
     expansion = 4
 
-    def __init__(self, conv, in_channels, out_channels, stride=1):
+    def __init__(self, in_channels, out_channels, stride=1):
         super.__init__()
         self.residual_function = nn.Sequential(
-            conv(in_channels, out_channels, kernel_size=1, bias=False),
+            DSC(in_channels=in_channels, out_channels=out_channels, kernel_size=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(),
-            conv(out_channels, out_channels, stride=stride, padding=1, bias=False),
+            DSC(in_channels=out_channels, out_channels=out_channels, stride=stride, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(),
-            conv(out_channels, out_channels * BottleNeck.expansion, kernel_size=1, bias=False),
+            DSC(in_channels=out_channels, out_channels=out_channels * BottleNeck.expansion, kernel_size=1, bias=False),
             nn.BatchNorm2d(out_channels * BottleNeck.expansion),
         )
 
@@ -30,32 +63,48 @@ class BottleNeck(nn.Module):
 
         if stride != 1 or in_channels != out_channels * BottleNeck.expansion:
             self.shortcut = nn.Sequential(
-                conv(in_channels, out_channels * BottleNeck.expansion, stride=stride, kernel_size=1, bias=False),
+                DSC(in_channels=in_channels, out_channels=out_channels * BottleNeck.expansion, stride=stride, kernel_size=1, bias=False),
                 nn.BatchNorm2d(out_channels * BottleNeck.expansion),
             )
 
     def forward(self, x):
         x = self.residual_function(x) + self.shortcut(x)
         return self.relu(x)
+'''
 
 class MyModel(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, block, num_block, num_classes=100):
         super(MyModel, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(32 * 4 * 4, 128)
-        self.fc2 = nn.Linear(128, num_classes)
-        self.dropout = nn.Dropout()
+        self.in_channels = 16
+
+        self.conv1 = nn.Sequential(
+            DSC(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
+        )
+
+        self.conv2_x = self._make_layer(block, 32, num_block[0], 1)
+        self.conv3_x = self._make_layer(block, 64, num_block[1], 2)
+        self.conv4_x = self._make_layer(block, 128, num_block[2], 2)
+        self.conv5_x = self._make_layer(block, num_classes, num_block[3], 2)
+
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels
+
+        return nn.Sequential(*layers)
 
     def forward(self, x):
-        x = self.pool(self.relu(self.conv1(x)))
-        x = self.pool(self.relu(self.conv2(x)))
-        x = self.pool(self.relu(self.conv3(x)))
-        x = x.reshape(-1, 32 * 4 * 4)
-        x = self.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
-        return x
+        x = self.conv1(x)
+        x = self.conv2_x(x)
+        x = self.conv3_x(x)
+        x = self.conv4_x(x)
+        x = self.conv5_x(x)
+        x = self.avg_pool(x)
+        return x.view(x.size(0), -1)
--- train.py
+++ train.py
@@ -22,10 +22,10 @@ train_dataset = TensorDataset(train_images_tensor, train_labels_tensor)
 train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
 
 num_classes = len(np.unique(train_labels))
-model = MyModel(num_classes)
+model = MyModel([2, 2, 1, 1], num_classes)
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
+optimizer = optim.AdamW(model.parameters(), lr=0.001, weight_decay=0.01)
+scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=1000)
 
 if not torch.cuda.is_available():
     print("CUDA is disabled")
@@ -33,13 +33,15 @@ if not torch.cuda.is_available():
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
-num_epochs = 10
-model_save_path = repository_path + "weight/final_model_weights.pth"
+num_epochs = 1000
+model_save_path = repository_path + "weight/model_weights"
 
 model.train()
 
 for epoch in range(num_epochs):
     running_loss = 0.0
+    running_correct = 0
+    total_samples = 0
     for images, labels in tqdm(train_loader):
         images = images.permute(0, 3, 1, 2)
         images, labels = images.to(device), labels.to(device).long()
@@ -53,9 +55,17 @@ for epoch in range(num_epochs):
 
         running_loss += loss.item()
 
+        _, preds = torch.max(outputs, 1)
+        running_correct += torch.sum(preds == labels).item()
+        total_samples += labels.size(0)
+
     scheduler.step()
     train_loss =  running_loss / len(train_loader)
-    print(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {train_loss:.4f}")
+    train_accuracy = running_correct / total_samples
+    print(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {train_loss:.4f}, Train Accuracy: {train_accuracy:.4f}")
 
-torch.save(model.state_dict(), model_save_path)
-print(f"Model weights saved to {model_save_path}.")
+    if (epoch+1) % 50 == 0:
+        tmp_save_path = model_save_path + f"{epoch+1}.pth"
+        torch.save(model.state_dict(), tmp_save_path)
+        print(f"Model weights saved to {tmp_save_path}.")
+
