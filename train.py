09c3c1367ac84a2c57b0ba56cf112a1d2c2821cd
dh610 <ddhhyy2000@gmail.com>
2024-10-14 20:58:33 +0900

    train

--- train.py
+++ train.py
@@ -23,6 +23,7 @@ augmentation = transforms.Compose([
     transforms.RandomHorizontalFlip(p=0.5),
     transforms.RandomRotation(degrees=15),
     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+    transforms.RandomCrop(32, padding=4),
     transforms.ToTensor(),  # [0, 255] → [0, 1]
     transforms.Normalize(mean=mean, std=std),
 ])
