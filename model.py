d8dc99ac5c59f251d7de87ab78ab6f660f16dec5
dh610 <ddhhyy2000@gmail.com>
2024-10-15 17:01:19 +0900

    test

--- model.py
+++ model.py
@@ -147,7 +147,7 @@ class MyNet(nn.Module):
 
         # Stacked ShuffleNetV2 blocks for different stages
         self.stage2 = self._make_stage(out_channels[0], out_channels[1], 5)
-        self.stage3 = self._make_stage(out_channels[1], out_channels[2], 10)
+        self.stage3 = self._make_stage(out_channels[1], out_channels[2], 11)
         self.stage4 = self._make_stage(out_channels[2], out_channels[3], 5)
 
         # Final 1x1 convolution layer before classification
