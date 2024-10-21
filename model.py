494ba1314852466db55b98ff6cb18dfc4138f769
dh610 <ddhhyy2000@gmail.com>
2024-10-21 13:19:32 +0900

    model

--- model.py
+++ model.py
@@ -58,8 +58,8 @@ class NeoResNet(nn.Module):
 
         self.conv1 = self._make_layer(block, 4, 4)
         self.conv1_x = self._make_layer(block, 8, 4)
-        self.conv2_x = self._make_layer(block, 16, 3)
-        self.conv3_x = self._make_layer(block, 32, 4)
+        self.conv2_x = self._make_layer(block, 16, 4)
+        self.conv3_x = self._make_layer(block, 32, 3)
         self.conv4_x = self._make_layer(block, 64, 4)
         self.conv5_x = self._make_layer(block, 128, 1)
 
